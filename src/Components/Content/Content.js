33ce82e3ae9567b7344753ff5c3fce87abd6ed7a
Ermakov Nikita <vienn1009@gmail.com>
2023-09-20 23:38:14 +0300

    scroll button and focus checkbox

--- src/Components/Content/Content.js
+++ src/Components/Content/Content.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Row, Col } from 'antd';
 
 import styles from './Content.module.scss';
@@ -6,6 +6,26 @@ import TicketTabs from './TicketTabs/TicketTabs';
 import TicketFilter from './TicketFilter/TicketFilter';
 
 export default function Content() {
+  const [buttonStyle, setButtonStyle] = useState({});
+
+  const handleScroll = () => {
+    const scroll = window.scrollY;
+    const newButtonStyle = {
+      display: scroll > 200 ? 'block' : 'none',
+    };
+    setButtonStyle(newButtonStyle);
+  };
+  const handleTop = () => {
+    window.scrollTo(0, 0);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <main className={styles.content}>
       <Row className={styles.row}>
@@ -15,6 +35,17 @@ export default function Content() {
         <Col lg={16} className={styles.colright}>
           <TicketTabs />
         </Col>
+
+        <button
+          className={styles.onTop}
+          type="button"
+          id="myBtn"
+          style={buttonStyle}
+          onClick={handleTop}
+          onScroll={handleScroll}
+        >
+          go to top
+        </button>
       </Row>
     </main>
   );
